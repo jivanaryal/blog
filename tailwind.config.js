4df82d4fe1bcab5965b480b0919a04e6e140059d
jivan aryal <jeevanaryal7947@gmail.com>
2023-04-04 20:24:14 +0545

    hello

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      screens: {
-        pd: "374px",
-
-        sm: "640px",
-
-        md: "768px",
-
-        lg: "1024px",
-
-        xl: "1280px",
-
-        "2xl": "1536px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
