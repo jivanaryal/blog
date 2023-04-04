ff0f1991e86f65b2f0ddcfdd4dd8526d9309c41b
jivan aryal <jeevanaryal7947@gmail.com>
2023-04-04 15:29:15 +0545

    hello

--- src/Hoc/Layout/Layout.jsx
+++ src/Hoc/Layout/Layout.jsx
@@ -1,36 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 // import Login2 from "../../Components/Login2";
 import Sidebar from "../../Components/Navigation/Sidebar/Sidebar";
 import DownNav from "../../Components/Navigation/Toolbar/DownNav";
-// import TopNav from "../../Components/Navigation/Toolbar/TopNav";
+import TopNav from "../../Components/Navigation/Toolbar/TopNav";
 
 const Layout = ({ children }) => {
   const [show, setShow] = useState(false);
-  // const [scroll, setScroll] = useState(false);
+  const [scroll, setScroll] = useState(false);
   const location = useLocation();
   console.log(location);
-  // const handleScroll = () => {
-  //   console.log(window.scrollY);
-  //   if (window.scrollY > 10) {
-  //     setScroll(true);
-  //   } else {
-  //     setScroll(false);
-  //   }
-  // };
-  // useEffect(() => {
-  //   window.addEventListener("scroll", handleScroll);
+  const handleScroll = () => {
+    console.log(window.scrollY);
+    if (window.scrollY > 10) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
 
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div className="">
       <div className="">
-        {/* <TopNav scroll={scroll} /> */}
+        <TopNav scroll={scroll} />
         <DownNav show={show} setShow={setShow} />
         <Sidebar show={show} setShow={setShow} />
         <div>{children}</div>
